50b5a49908d0a82f06c10566155da6e655b29677
Leonardo Chinarelo <leonardo.chinarelo@hotmail.com>
2023-09-12 11:15:38 -0300

    Criando Work Queues

---
+++ Tutorial 01/Receive/Receive.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("hello", false, false, false, null);
+Console.WriteLine("[*] Aguardando mensagens.");
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channel.BasicConsume("hello", true, consumer);
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ Tutorial 01/Send/Send.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("hello", false, false, false, null);
+const string message = "hello world";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(string.Empty, "hello", null, body);
+Console.WriteLine($"[x] Sent {message}");
+Console.WriteLine($"Press [enter] to exit");
+Console.ReadLine();
